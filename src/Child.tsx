54c7242fcecaa3f1e1e259e9c099e0898495a97b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-23 14:40:22 +0530

    props in react

--- src/Child.tsx
+++ src/Child.tsx
@@ -1,8 +1,18 @@
-const Child = (props) => {
+const Child = (props: any) => {
+
+    const childData = "Monu";
+
+    const sendData = () => {
+        console.log("child function called.");
+        props.sendDataToParent(childData);
+    };
+
     return (
         <>
             <h2>Child Component</h2>
             <p>Parent data in child component: {props.dataFromParent}</p>
+            <p>Child data in child component: {childData}</p>
+            <button onClick={sendData}>Send child data to parent</button>
 
         </>
     );
--- src/Parent.tsx
+++ src/Parent.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Child from "./Child";
 const Parent = () => {
     const parentData = "Sonu";
+    const [childDataInParent, setChildDataInParent] = useState("");
+
+    const getDataFromChild = (data: string) => {
+        console.log("parent function called.");
+        setChildDataInParent(data);
+    };
+
     return (
         <>
             <h2>Parent Component</h2>
-    <p>Parent data in parent component: {parentData}</p>
-            <Child dataFromParent={parentData} />
+            <p>Parent data in parent component: {parentData}</p>
+            <p>Child data in parent component: {childDataInParent}</p>
+            <Child dataFromParent={parentData} sendDataToParent={getDataFromChild} />
         </>
     );
 };
